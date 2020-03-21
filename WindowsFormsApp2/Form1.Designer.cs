6fe97e8c66349d82eb7019a8a24b66fab3d09911
SuperNoice <SuperNoice@yandex.ru>
2020-03-21 09:54:04 +0300

    init

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Смежность вершин";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(177, 157);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(494, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Кнопка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(483, 363);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Матрица смежности вершин и ребер";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Смежность ребер";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(267, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Список смежности";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(231, 45);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(175, 157);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 235);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(177, 148);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(231, 235);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(175, 148);
+            this.richTextBox4.TabIndex = 9;
+            this.richTextBox4.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(204, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "V\\E";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(668, 385);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+
+
+    public partial class Form1 : Form
+    {
+        struct Edge
+        {
+            public
+            int TransmitterVertex, ReceiverVertex;
+
+            public Edge(int trans, int rec)
+            {
+                this.TransmitterVertex = trans;
+                this.ReceiverVertex = rec;
+            }
+
+            public void SetTransmitter(int newValue)
+            {
+                this.TransmitterVertex = newValue;
+            }
+
+            public void SetReceiver(int newValue)
+            {
+                this.ReceiverVertex = newValue;
+            }
+
+            public int GetTransmitter()
+            {
+                return this.TransmitterVertex;
+            }
+
+            public int GetReceiver()
+            {
+                return this.ReceiverVertex;
+            }
+
+            public void EdgeSetup(int value1, int value2)
+            {
+                this.TransmitterVertex = value1;
+                this.ReceiverVertex = value2;
+            }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Normal(string str)
+        {
+            string res = "";
+            bool f = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (f)
+                {
+                    if (str[i] == '-') continue;
+                    res += str[i];
+                }
+                if (str[i] == ':') f = true;
+            }
+
+            return res;
+        }
+
+        bool Provsm(List<Edge> list, int st, int str)
+        {
+            bool f = false;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].GetTransmitter() == st && list[i].GetReceiver() == str) f = true;
+                if (list[i].GetReceiver() == st && list[i].GetTransmitter() == str) f = true;
+            }
+
+            return f;
+        }
+        void Fill_all(int[,] mas1)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            richTextBox4.Clear();
+            textBox1.Clear();
+
+            int size = Convert.ToInt32(Math.Sqrt(mas1.Length));
+
+
+            for (int i = 0; i < size; i++)                          //RTB1
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    richTextBox1.AppendText(Convert.ToString(mas1[i, j]) + " ");
+                }
+                richTextBox1.AppendText("\n");
+            }
+            richTextBox1.Undo();
+
+            // RTB4
+
+            for (int i = 0; i < size; i++)
+            {
+                richTextBox4.AppendText(i + ":");
+                for (int j = 0; j < size; j++)
+                {
+                    if (mas1[i, j] == 1) { richTextBox4.AppendText(Convert.ToString(j)); richTextBox4.AppendText("->"); }
+                }
+                richTextBox4.Undo();
+                richTextBox4.AppendText("\n");
+            }
+            richTextBox4.Undo();
+
+
+            List<Edge> list = new List<Edge>();         // RTB2
+
+
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (mas1[i, j] == 1 && Provsm(list, i, j)) { Edge tmp = new Edge(); tmp.EdgeSetup(i, j); list.Add(tmp); }
+
+            int st = list.Count, str = size;
+
+            int[,] rv = new int[st, str];
+
+            for (int i = 0; i < st; i++)
+            {
+                rv[list[i].GetTransmitter(), i] = 1;
+                rv[list[i].GetReceiver(), i] = 1;
+            }
+
+            for (int i = 0; i < str; i++)
+            {
+                for (int j = 0; j < st; j++)
+                {
+                    richTextBox2.AppendText(Convert.ToString(rv[i, j]));
+                }
+                richTextBox2.AppendText("\n");
+            }
+            richTextBox2.Undo();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] mas1, mas2, mas3, mas4;
+            string[] str1, str2;
+            bool f = true;
+
+            if (richTextBox1.Text.Length != 0 && f)
+            {
+                string str = richTextBox1.Text;
+                str1 = str.Split('\n');
+                str2 = str1[0].Split(' ');
+                mas1 = new int[str1.Length, str2.Length];
+
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    str2 = str1[i].Split(' ');
+                    for (int j = 0; j < str2.Length; j++)
+                    {
+                        try
+                        {
+                            mas1[i, j] = Convert.ToInt32(str2[j]);
+                            textBox1.Clear();
+                        }
+                        catch (Exception)
+                        {
+                            textBox1.Clear();
+                            textBox1.AppendText("Error");
+
+                        }
+                    }
+                }
+                Fill_all(mas1);
+                f = false;
+            }
+            if (richTextBox2.Text.Length != 0 && f)
+            {
+                string str = richTextBox1.Text;
+                str1 = str.Split('\n');
+                str2 = str1[0].Split(' ');
+                mas1 = new int[str1.Length, str1.Length];
+                List<int> pair = new List<int>();
+
+                for (int j = 0; j < str2.Length; j++)
+                {
+
+                    for (int i = 0; i < str1.Length; i++)
+                    {
+                        str2 = str1[i].Split(' ');
+                        if (str2[j] == "1") pair.Add(i);
+                    }
+
+                    mas1[pair[0], pair[1]] = 1;
+                    pair.Clear();
+                }
+                Fill_all(mas1);
+                f = false;
+            }
+            if (richTextBox3.Text.Length != 0 && f)
+            {
+                string str = richTextBox1.Text;
+                str1 = str.Split('\n');
+                str2 = str1[0].Split(' ');
+                mas3 = new int[str1.Length, str2.Length];
+
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    str2 = str1[i].Split(' ');
+                    for (int j = 0; j < str2.Length; j++)
+                    {
+
+                    }
+                }
+                f = false;
+            }
+            if (richTextBox4.Text.Length != 0 && f)
+            {
+                string str = richTextBox4.Text;
+                str1 = str.Split('\n');
+
+                mas1 = new int[str1.Length, str1.Length];
+
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    str1[i] = Normal(str1[i]);
+                    str2 = str1[i].Split('>');
+                    for (int j = 0; j < str2.Length; j++)
+                    {
+                        mas1[i, Convert.ToInt32(str2[j])] = 1;
+                        mas1[Convert.ToInt32(str2[j]), i] = 1;
+                    }
+                }
+                Fill_all(mas1);
+                textBox1.AppendText("ok");
+                f = false;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            richTextBox4.Clear();
+            textBox1.Clear();
+        }
+    }
+}
